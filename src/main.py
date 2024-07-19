a70f9af8bd4bd8f8bd7e2014a5e995a298f514d5
timo.bergen <timo.bergen@camao-tec.com>
2024-07-19 21:38:17 +0200

    chore: final final fix config, now its working :)

--- src/main.py
+++ src/main.py
@@ -13,8 +13,8 @@ def resolve_dyndns():
     try:
         resolved_ip = socket.gethostbyname(dyndns_domain)
         with open('/app/dyndns.conf', 'w') as f:
-            f.write(f"@allowed_ip remote_ip {resolved_ip}\n")
-            f.write("abort not @allowed_ip\n")
+            f.write(f"@allowed_ip not remote_ip {resolved_ip}\n")
+            f.write("abort @allowed_ip\n")
         print(f"Resolved {dyndns_domain} to {resolved_ip} and updated configuration. Will retry in 5 minutes.")
     except socket.gaierror as e:
         print(f"Error resolving {dyndns_domain}: {e}")
